0b6f15f2962d80d5383c0a0dd51f1bb63795bab5
ming0604 <b0981340647@gmail.com>
2023-09-26 23:28:30 +0800

    constructor test

--- happy.cpp
+++ happy.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+
+class test
+{
+    private:
+        int a;
+        int b;
+        int c;
+    public:
+        test(int a, int b, int c);
+
+};
+test::test(int a=10, int b=20, int c=30):a(a),b(b),c(c)
+{
+}
+
 int main()
 {
-    cout<<"hello world,happy"<<endl;
+
+    test test1(1,3);
+   
 }
+
